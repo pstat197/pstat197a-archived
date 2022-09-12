452052e1e10f4d2588d0595b300046f300ad2d9c
tdr <tdr@ucsb.edu>
2022-09-11 18:07:10 -0700

    move data in lab1

--- materials/labs/lab1-setup/scripts/data-retrieval.R
+++ materials/labs/lab1-setup/scripts/data-retrieval.R
@@ -1,7 +1,7 @@
 library(tidyverse)
 
 # retrieve pollution data
-url <- 'https://raw.githubusercontent.com/pstat197/pstat197a/main/materials/data/pollution.csv'
+url <- 'https://raw.githubusercontent.com/pstat197/pstat197a/main/materials/labs/lab1-setup/data/pollution.csv'
 pollution <- read_csv(url)
 
 # write as csv to file
